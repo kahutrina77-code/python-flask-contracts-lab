b695f73e6ca524c6fbdd8b5191cbe99419e8222d
trinaluseno <kahutalia77@gmail.com>
2026-03-11 21:41:37 +0300

    add request-response

--- server/app.py
+++ server/app.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, current_app, g, make_response
+from flask import Flask, request, current_app, g, make_response, jsonify
 
 contracts = [{"id": 1, "contract_information": "This contract is for John and building a shed"},{"id": 2, "contract_information": "This contract is for a deck for a buisiness"},{"id": 3, "contract_information": "This contract is to confirm ownership of this car"}]
 customers = ["bob","bill","john","sarah"]
 app = Flask(__name__)
+
+@app.route('/contract/<int:id>')
+def contract_information(id):
+    contract = next((c for c in contracts if c["id"] == id), None)
+    if contract:
+        return make_response(jsonify(contract), 200)
+    else:
+        return make_response("Contract not found", 404)
+
+@app.route('/customer/<customer_name>')
+def customer_information(customer_name):
+    if customer_name in customers:
+        return make_response('', 204)
+    else:
+        return make_response("Customer not found", 404)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
